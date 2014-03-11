7400624455d9f73b7bc58db95928b0c0ff250f44
nwittstock <nwittstock@north.com>
2014-03-10 20:54:43 -0700

    Begin separating into module.

---
+++ locales/en.js
@@ -0,0 +1,11 @@
+{
+	"north": "north",
+	"south": "south",
+	"east": "east",
+	"get": "get",
+	"west": "west",
+	"You can't find a \"%s\" in this room!": "You can't find a \"%s\" in this room!",
+	"go": "go",
+	"exits": "exits",
+	"Sorry, I don't know how to \"%s\" in this room.": "Sorry, I don't know how to \"%s\" in this room."
+}
